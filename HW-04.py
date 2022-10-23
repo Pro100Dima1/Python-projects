23901d6a14ebde1edcace8a3ae371e4fb46d784a
Pro100Dima1 <dima.sinkin@yandex.ru>
2022-10-23 22:55:41 +0300

    fix

--- HW-04.py
+++ HW-04.py
@@ -6,10 +6,10 @@ if count_ticket < 3:
     if age < 18:
         free = 0
         print("Бесплатно")
-    elif 18 < age < 25:
+    elif 18 <= age < 25:
         price = 990 * count_ticket
         print(price)
-    elif age > 25:
+    elif age >= 25:
          full_price = 1390 * count_ticket
          print(full_price)
 else:
