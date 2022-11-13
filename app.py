0049622beda0727a18ddb8a1563743df9aa5b169
Pro100Dima1 <dima.sinkin@yandex.ru>
2022-11-13 13:11:55 +0300

    Add some fix

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def convert(message: telebot.types.Message):
             raise ConvertionException("Слишком много параметров")
 
         quote, base, amount = values
-        total_base = CryptoConverter.convert(quote, base, amount)
+        total_base = CryptoConverter.get_price(quote, base, amount)
     except ConvertionException as e:
         bot.reply_to(message, f'Ошибка пользователя \n{e}')
     except Exception as e:
